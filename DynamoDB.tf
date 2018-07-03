fb4cb7858d59a35320378f7f35ada68e49675d6a
navgit <mail.navazish@gmail.com>
2018-07-03 07:52:54 +0000

    Pushing the code

--- DynamoDB.tf
+++ DynamoDB.tf
@@ -3,7 +3,7 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "UserId"
-  range_key      = "462281"
+  
   
   attribute {
     name = "UserId"
